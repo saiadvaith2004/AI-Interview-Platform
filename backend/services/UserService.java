351345b936a8e994302c294eac7eb8be3dabb4a6
Nagulapally Sai Advaith <advaithsai.n112@gmail.com>
2026-03-19 22:03:06 +0530

    Initial backend structure and important files for Spring Boot application.

---
+++ backend/services/UserService.java
@@ -0,0 +1,22 @@
+package com.example.services;
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
