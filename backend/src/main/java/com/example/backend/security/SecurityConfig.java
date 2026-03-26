ee83785f4e8cefa01e9e08edc368b6db98db63d3
saiadvaith2004 <advaithsai.n112@gmail.com>
2026-03-26 15:05:06 +0530

    Added error statement for debugging on UI

--- backend/src/main/java/com/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/security/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import java.beans.Customizer;
+import org.springframework.security.config.Customizer;
 import java.util.Arrays;
 import java.util.List;
 
